4a42e7cea6a376cb0fa53260295f71e41752c18c
jasonfangmagic <jasonfangmagic@gmail.com>
2024-04-15 12:08:33 -0400

    add story

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,13 +14,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let example = [
+    Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."),
+    Story(title: "You see a tiger", choice1: "Shout for help", choice2: "Play Dead"),
+    Story(title: "You find a treasure chest", choice1: "Open it ", choice2: "Check for traps")
+    ]
+    
+    var storyNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = "You see a fork in the road."
-        choice1Button.setTitle("Take a left.", for: .normal)
-        choice1Button.setTitle("Take a right.", for: .normal)
-
+        updateUI()
+    }
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle
+        let actualStory = example[storyNumber]
+        let actualChoice1 = actualStory.choice1
+        let actualChoice2 = actualStory.choice2
+        
+        if userChoice == actualChoice1 {
+            storyNumber = 1
+        } else {
+            storyNumber = 2
+        }
+        
+        updateUI()
+        
+    }
+    
+    
+    func updateUI() {
+        storyLabel.text = example[storyNumber].title
+        choice1Button.setTitle(example[storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(example[storyNumber].choice2, for: .normal)
     }
 
 
